ecfe9a0ea226c4010546573001d67afcb91ca902
saksham <saksham4200@gmail.com>
2023-07-07 05:52:15 +0530

    full stack food ordering website made in react and nodejs

---
+++ components/footer.js
@@ -0,0 +1,18 @@
+export const footer = () => {
+    return (
+        `<div id="footer">
+            <p class="text-center">Made with ❤️ by Saksham Pandey</p>
+            <div>
+                <button class="btn btn-primary">
+                    <a href="https://www.linkedin.com/in/saksham-pandey21/" target="_blank">LinkedIn</a>
+                </button>
+                <button class="btn btn-success">
+                    <a href="https://github.com/Saksham21s" target="_blank">GitHub</a>
+                </button>
+                <button class="btn btn-danger">
+                    <a href="https://portfolio-github-io-alpha.vercel.app/" target="_blank">Portfolio</a>
+                </button>
+            </div>
+        </div>`
+    );
+};
